1c2870dd9365615aec68ef2ccbe116d388a3dc51
tai2 <hello@tai2.net>
2024-02-07 22:36:41 +0900

    Add TLS configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: "local.autify.com",
+  },
+  plugins: [
+    react(),
+    basicSsl({
+      name: 'test',
+      domains: ['local.autify.com'],
+    })
+  ],
 })
